f8ca7750df2ddab3698739b7aa7305525273098a
Mleczuuu <mleczutokozak@gmail.com>
2022-04-19 21:44:10 +0200

    switch to 4.4.0 LTS

--- src/main/java/com/github/mleczu/jdaspigot/Version.java
+++ src/main/java/com/github/mleczu/jdaspigot/Version.java
@@ -1,5 +1,5 @@
 package com.github.mleczu.jdaspigot;
 
 public class Version {
-    public static final String JDAVersion = "JDA 5.0.0-alpha.9";
+    public static final String JDAVersion = "JDA 4.4.0";
 }
